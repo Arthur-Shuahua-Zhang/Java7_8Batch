6250175f797d14f9231e30583509595e94063fa5
Arthur-Shuahua-Zhang <shaohua.zhang@antra.com>
2022-03-29 14:50:59 -0400

    add pirnt out

--- src/main/java/com/antra/MyFirstClass.java
+++ src/main/java/com/antra/MyFirstClass.java
@@ -2,8 +2,64 @@ package com.antra;
 
 public class MyFirstClass {
     public static void main(String[] args) {
-        // this is print out
-        System.out.println("Hello new batch!");
+//        // this is print out
+//        System.out.println("Hello new batch!");
+//
+//        int a = 1;
+//        Integer b = new Integer(4);
+//        System.out.println(a + b);
+//        String s = "1_";
+//        StringBuilder  sb1 = new StringBuilder("2_");
+//        StringBuffer sb2 = new StringBuffer("3_");
+//
+//        cancat1(s);
+//        cancat2(sb1);
+//        cancat3(sb2);
+//
+//        System.out.println(s);
+//        System.out.println(sb1.toString());
+//        System.out.println(sb2.toString());
+
+        String s1 = "Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111";
+        String s2 = "Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111Hello1111";
+        System.out.println(s1 == s2);
+//
+//        String s3 = new String("abc");
+//        System.out.println(s1 == s3);
+//
+//        String s4 = new String("abc");
+//        System.out.println(s3 == s4);
+
+//        Integer a = 10;
+//        Integer b = 10;
+//        Integer c = new Integer(10);
+//        System.out.println(a == b);
+//        System.out.println(a == c);
+
+//        Integer d = 128;
+//        Integer e = 128;
+//        System.out.println(d == e);
+
+
+
 
     }
+
+
+
+
+
+//
+//    public static void cancat1(String s) {
+//        s = s + "abc";
+//    }
+//
+//    public static void cancat2(StringBuilder s) {
+//        s = s.append("abc");
+//    }
+//
+//    public static void cancat3(StringBuffer s) {
+//        s = s.append("abc");
+//    }
+
 }
