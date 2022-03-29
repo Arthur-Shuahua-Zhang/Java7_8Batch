f765869500ecb78f95ce6205841d5ee8ab76ea06
Arthur-Shuahua-Zhang <shaohua.zhang@antra.com>
2022-03-29 14:06:28 -0400

    add pirnt out

--- src/main/java/com/antra/MyFirstClass.java
+++ src/main/java/com/antra/MyFirstClass.java
@@ -2,6 +2,8 @@ package com.antra;
 
 public class MyFirstClass {
     public static void main(String[] args) {
+        // this is print out
         System.out.println("Hello new batch!");
+
     }
 }
